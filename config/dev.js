56643babac4d10fb8d56993556ac581595e537e8
Sean Nilan <sean.nilan@gmail.com>
2021-01-07 19:47:01 -0500

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  redisUrl: 'redis://127.0.0.1:6379',
-  mongoURI: 'mongodb://user:Gbaltar7!@cluster0-shard-00-00.w1mtr.mongodb.net:27017,cluster0-shard-00-01.w1mtr.mongodb.net:27017,cluster0-shard-00-02.w1mtr.mongodb.net:27017/blog?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123'
-};
